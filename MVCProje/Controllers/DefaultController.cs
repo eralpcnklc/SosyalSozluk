3511a9b1cadfbe54eaa6996b14e533d95e4cf754
ECINKILIC <eralp.cnklc@gmail.com>
2024-09-09 11:30:32 +0300

    giriş yapmayan kullanıcılar için başlıkların solda olduğu bir vitrin sayfası eklendi

---
+++ MVCProje/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
+        public ActionResult Headings() {
+            var headingList = hm.GetList();
+            return View(headingList);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
